56758d0f28e8d3290090871fd52c55b82ed5d937
Brendan Morrison <b.k.morrison2@gmail.com>
2021-03-19 14:43:13 -0700

    metamask integration

--- Website-old/script.js
+++
@@ -1,5 +0,0 @@
-//let Web3 = require('web3');
-import Web3 from 'web3'
-
-
-
--- cryptonauts-app/src/App.js
+++ cryptonauts-app/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React ,{useEffect, useState} from 'react';
+import Web3 from 'web3';
 
 function App() {
+
+  useEffect(() => {
+    loadWeb3();
+    loadBlockchainData();
+  }, [])
+
+  const loadWeb3 = async () => {
+    if(window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      window.alert(
+        'Non-Ethereum browser detected.'
+      );
+    }
+  };
+
+  const loadBlockchainData = async () => {
+    const web3 = window.web3;
+
+    const accounts = web3.eth.getAccounts();
+    const account = accounts[0];
+
+    const networkId = await web3.eth.net.getId();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = 'container'>
+      <h1>Cryptonauts </h1>
     </div>
-  );
+
+    );
 }
 
 export default App;
