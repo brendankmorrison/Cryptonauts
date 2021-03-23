a76487bc15bbcd1cbc0bc44be123ecf3f36bf1d8
Brendan Morrison <b.k.morrison2@gmail.com>
2021-03-23 11:07:13 -0700

    added search button

--- cryptonauts-app/src/components/pages/gallery.js
+++ cryptonauts-app/src/components/pages/gallery.js
@@ -3,10 +3,21 @@ import CryptonautCard from './PageItems/CryptonautCard';
 import './Gallery.css';
 
 function Gallery (props){
+    // showRandom cryptonaut state or searchAddress state
+    const [state, setState] = useState('showRandom')
+
+    useEffect(() => {
+
+
+    }, [])
 
     return(
         <div className = 'galleryContainer'>
-            <div className = 'inputContainer'><input className = 'searchAddress' placeholder = 'SEARCH ADDRESS' type = 'search'/></div>
+            <div className = 'inputContainer'>
+                <input className = 'searchAddress' placeholder = 'SEARCH ADDRESS' type = 'search'/>
+                <div className = 'searchButton rounded-pill'> SEARCH</div>
+                
+            </div>
             <CryptonautCard/>
             <div className = 'cardInfo'> OWNER: 0x72502738CA11AEC18deA56E2Ce21ab2caC875e66 </div>
             <div className = 'nextImage rounded-pill'> 
