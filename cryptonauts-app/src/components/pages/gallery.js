b7f85ccafc75957d3cae24c54a2e8f7d65a3f7a0
Brendan Morrison <b.k.morrison2@gmail.com>
2021-03-23 10:42:31 -0700

    added address input

--- cryptonauts-app/src/components/pages/gallery.js
+++ cryptonauts-app/src/components/pages/gallery.js
@@ -6,6 +6,7 @@ function Gallery (props){
 
     return(
         <div className = 'galleryContainer'>
+            <div className = 'inputContainer'><input className = 'searchAddress' placeholder = 'SEARCH ADDRESS' type = 'search'/></div>
             <CryptonautCard/>
             <div className = 'cardInfo'> OWNER: 0x72502738CA11AEC18deA56E2Ce21ab2caC875e66 </div>
             <div className = 'nextImage rounded-pill'> 
